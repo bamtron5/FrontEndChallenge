878154468dec380a7dcf9e5369ec4115b6738fd8
Brandon Morrison <brmorrison61@gmail.com>
2015-01-24 20:56:31 -0800

    finished the app portion of the project

--- js/app.js
+++ js/app.js
@@ -1,114 +1,11 @@
-var app = angular.module("gitHubSearch", []);
+var app = angular.module("gitHubSearch", ['ngMaterial']);
 
-app.controller('ghsController', function($scope, $http, $q){
-	$scope.objs = {
-  "repositories": [
-    {
-      "type": "repo",
-      "username": "karan",
-      "name": "Projects-Solutions",
-      "owner": "karan",
-      "homepage": "",
-      "description": ":pager: Links to other users' solutions to Projects (https://github.com/karan/Projects/)",
-      "language": "",
-      "watchers": 654,
-      "followers": 654,
-      "forks": 352,
-      "size": 1805,
-      "open_issues": 0,
-      "score": 19.367487,
-      "has_downloads": true,
-      "has_issues": true,
-      "has_wiki": true,
-      "fork": false,
-      "private": false,
-      "url": "https://github.com/karan/Projects-Solutions",
-      "created": "2013-07-31T13:05:16Z",
-      "created_at": "2013-07-31T13:05:16Z",
-      "pushed_at": "2015-01-24T18:11:13Z",
-      "pushed": "2015-01-24T18:11:13Z"
-    },
-    {
-      "type": "repo",
-      "username": "jcooky",
-      "name": "karan",
-      "owner": "karan",
-      "homepage": null,
-      "description": "Karan Project",
-      "language": "Java",
-      "watchers": 0,
-      "followers": 0,
-      "forks": 0,
-      "size": 304,
-      "open_issues": 0,
-      "score": 15.015927,
-      "has_downloads": true,
-      "has_issues": true,
-      "has_wiki": true,
-      "fork": false,
-      "private": false,
-      "url": "https://github.com/jcooky/karan",
-      "created": "2012-09-24T06:42:50Z",
-      "created_at": "2012-09-24T06:42:50Z",
-      "pushed_at": "2013-02-28T05:32:44Z",
-      "pushed": "2013-02-28T05:32:44Z"
-    },
-    {
-      "type": "repo",
-      "username": "karan3",
-      "name": "karan",
-      "owner": "karan3",
-      "homepage": null,
-      "description": "k",
-      "language": "",
-      "watchers": 0,
-      "followers": 0,
-      "forks": 0,
-      "size": 48,
-      "open_issues": 0,
-      "score": 15.015927,
-      "has_downloads": true,
-      "has_issues": true,
-      "has_wiki": true,
-      "fork": false,
-      "private": false,
-      "url": "https://github.com/karan3/karan",
-      "created": "2012-09-24T13:37:13Z",
-      "created_at": "2012-09-24T13:37:13Z",
-      "pushed_at": "2012-09-24T13:37:13Z",
-      "pushed": "2012-09-24T13:37:13Z"
-    },
-    {
-      "type": "repo",
-      "username": "kkishan",
-      "name": "karan",
-      "owner": "kkishan",
-      "homepage": null,
-      "description": "",
-      "language": "",
-      "watchers": 0,
-      "followers": 0,
-      "forks": 0,
-      "size": 112,
-      "open_issues": 0,
-      "score": 15.015927,
-      "has_downloads": true,
-      "has_issues": true,
-      "has_wiki": true,
-      "fork": false,
-      "private": false,
-      "url": "https://github.com/kkishan/karan",
-      "created": "2012-12-22T14:04:11Z",
-      "created_at": "2012-12-22T14:04:11Z",
-      "pushed_at": "2012-12-22T14:04:11Z",
-      "pushed": "2012-12-22T14:04:11Z"
-    }
-    ]
-}
-
-	$scope.results = $scope.objs["repositories"];
-	
-
+app.controller('ghsController', function($scope, $http){
+	$scope.fetch = function(){
+		$http.get('https://api.github.com/legacy/repos/search/' + $scope.search).success(function(data){
+			$scope.results = data["repositories"];
+		});	
+	}
 
     var indexedOwners = [];
     
@@ -122,18 +19,22 @@ app.controller('ghsController', function($scope, $http, $q){
         if (ownerIsNew) {
             indexedOwners.push(result.owner);
         }
-        console.log(indexedOwners);
         return ownerIsNew;
     }
-
-	/*$scope.fetch = function(){
-		$http.get('https://api.github.com/legacy/repos/search/' + $scope.search).success(function(data){
-			$scope.results = data["repositories"];
-		});	
-	}*/
+	
 })
 
-
+app.directive('mdItem', function() {
+    return {
+        restrict: 'E',
+        controller: function($scope, $element){
+         
+         $scope.toggleCards = function(){
+           $($element).toggleClass('open-cards');
+         }
+       }
+    };
+}); 
 
 
 
